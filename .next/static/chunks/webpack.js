e7df488735bec9fe26b299f5774a0af735da163b
David Tavares <davidtavares27@yahoo.com>
2022-01-24 18:42:58 -0300

    alterações da página

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e582d4b37345ab57"; }
+/******/ 		__webpack_require__.h = function() { return "55ed6576be2a7f49"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.885e5cc396289253.hot-update.js
+++ .next/static/webpack/webpack.885e5cc396289253.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a6f387b34f11bc18"; }
+/******/ 	__webpack_require__.h = function() { return "55ed6576be2a7f49"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
